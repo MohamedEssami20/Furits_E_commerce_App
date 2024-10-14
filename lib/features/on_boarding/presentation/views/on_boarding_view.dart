5ed7d5aa59a0510017c5ef734cdc8ec5810a61de
mohamed essam <mohamedessam2003147@gmail.com>
2024-10-14 16:05:39 +0200

    add some item to page view

--- lib/features/on_boarding/presentation/views/on_boarding_view.dart
+++ lib/features/on_boarding/presentation/views/on_boarding_view.dart
@@ -7,7 +7,8 @@ class OnBoardingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: OnBoardingViewBody(),
+      backgroundColor: Colors.white,
+      body: SafeArea(child: OnBoardingViewBody()),
     );
   }
 }
--- lib/features/on_boarding/presentation/views/widgets/page_view_item.dart
+++ lib/features/on_boarding/presentation/views/widgets/page_view_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PageViewItem extends StatelessWidget {
   const PageViewItem(
@@ -12,6 +13,36 @@ class PageViewItem extends StatelessWidget {
   final Widget title;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: MediaQuery.sizeOf(context).height * 0.5,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: SvgPicture.asset(
+                  backgroundImage,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: AspectRatio(
+                  aspectRatio: 1.9,
+                  child: SvgPicture.asset(image),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(18),
+                child: Text("تخط"),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
