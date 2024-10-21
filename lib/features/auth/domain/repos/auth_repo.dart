3b7ce6fda917aca8a8d07c6e5afa03107005a41f
mohamed essam <mohamedessam2003147@gmail.com>
2024-10-21 16:45:18 +0200

    ceate failure class and dartz package

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -1,7 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub/core/utils/failure.dart';
 import 'package:fruits_hub/features/auth/domain/entity/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<UserEntity> createUserWithEmailAndPassword(
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
       {required String email, required String password});
 }
  
