20f20ced9f955c5452483f9f5c5d9c754abc5587
mohamed essam <mohamedessam2003147@gmail.com>
2024-10-27 21:37:13 +0200

    create custom navigation bar

---
+++ lib/core/utils/Widgets/custom_buttom_navigation_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomButtomNavigationBar extends StatelessWidget {
+  const CustomButtomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      width: 350,
+      decoration: const ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          )),
+          shadows: [
+            BoxShadow(
+              color: Color(0x19000000),
+              blurRadius: 25,
+              offset: Offset(0, -2),
+              spreadRadius: 0,
+            ),
+          ]),
+    );
+  }
+}
