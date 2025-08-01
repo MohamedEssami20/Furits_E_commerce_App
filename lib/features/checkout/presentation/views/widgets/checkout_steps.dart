1864ce7527f10a8369eb354a487ebbdea86ed49c
mohamed <mohamedessam2003147@gmail.com>
2025-08-01 15:28:10 +0300

    create step item checkout to switch with active and inactive step item

--- lib/features/checkout/presentation/views/widgets/checkout_steps.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_steps.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fruits_hub/features/checkout/presentation/views/widgets/active_step_item.dart';
-import 'package:fruits_hub/features/checkout/presentation/views/widgets/in_active_step_item.dart';
+import 'package:fruits_hub/features/checkout/presentation/views/widgets/step_item.dart';
 
 class CheckoutSteps extends StatelessWidget {
   const CheckoutSteps({super.key});
@@ -11,8 +10,9 @@ class CheckoutSteps extends StatelessWidget {
       children: List.generate(
         getSteps().length,
         (index) => Expanded(
-          child: InActiveStepItem(
-            tiltle: getSteps()[index],
+          child: StepItem(
+            isActive: false,
+            title: getSteps()[index],
             index: (index + 1).toString(),
           ),
         ),
